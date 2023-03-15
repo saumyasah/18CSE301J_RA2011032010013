c341f4ac403a00ee785aa0bb4ba792094cb14da8
Saumya Sah <74109566+saumyasah@users.noreply.github.com>
2023-03-16 00:02:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,3 +12,11 @@ var barchart=svg.selectAll('rect')
     .attr("height",function(d){return d;})
     .attr("width", barwidth-barpadding)
     .attr("transform",function(d,i){var translate=[barwidth*i,0]; return "translate("+translate+")";});
+var text=svg.selectAll("text")
+    .data(dataset)
+    .enter()
+    .append("text")
+    .text(function(d){return d;})
+    .attr("y",function(d,i){return svgheight-d-2;})
+    .attr("x",function(d,i){return barwidth*i})
+    .attr("fill","#A64C38");
